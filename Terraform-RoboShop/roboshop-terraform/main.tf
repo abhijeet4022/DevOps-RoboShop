566bfc87f5c0b7600a5167031286b1fb1b35151c
Abhijeet Pramanik <abhijeet4022@gmail.com>
2024-04-11 20:16:24 +0530

    RoboShop-Project

--- Terraform-RoboShop/roboshop-terraform/main.tf
+++ Terraform-RoboShop/roboshop-terraform/main.tf
@@ -2,7 +2,7 @@ module "network" {
   source       = "../module/tf-module-vpc"
   for_each     = var.vpc
   vpc          = each.value["cidr"]
-  vpc_name     = each.value["name"]
+  vpc_name     = each.value["vpc_name"]
   project_name = var.project_name
 }
 
