fb0b0b7460f4979026560e57a6fe8a67897da2f8
Abhijeet Pramanik <abhijeet4022@gmail.com>
2024-04-12 17:43:04 +0530

    RoboShop-Project

--- Terraform-RoboShop/module/tf-module-vpc/subnets/main.tf
+++ Terraform-RoboShop/module/tf-module-vpc/subnets/main.tf
@@ -3,17 +3,17 @@ resource "aws_subnet" "main" {
   vpc_id            = var.vpc_id
   cidr_block        = each.value["cidr"]
   availability_zone = each.value["az"]
-  tags = {
-    Name = "${each.key}-subnet"
+  tags              = {
+    Name    = "${each.key}-subnet"
     Project = "roboshop"
   }
 }
 
 resource "aws_route_table" "main" {
-  for_each          = var.subnets
-  vpc_id = var.vpc_id
-  tags = {
-    Name = "${each.key}-tr"
+  for_each = var.subnets
+  vpc_id   = var.vpc_id
+  tags     = {
+    Name    = "${each.key}-tr"
     Project = "roboshop"
   }
 }
