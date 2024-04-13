c46009956e94414b62b5ad2b1fd4372c6a2fd655
Abhijeet Pramanik <abhijeet4022@gmail.com>
2024-04-13 11:43:07 +0530

    RoboShop-Project

--- Terraform-RoboShop/module/tf-module-vpc/main.tf
+++ Terraform-RoboShop/module/tf-module-vpc/main.tf
@@ -23,33 +23,29 @@ resource "aws_internet_gateway" "igw" {
 }
 
 resource "aws_route" "igw" {
-  for_each = lookup(lookup(module.subnets, "public", null), "route_table_ids", null)
-  route_table_id = each.value["id"]
+  for_each               = lookup(lookup(module.subnets, "public", null), "route_table_ids", null)
+  route_table_id         = each.value["id"]
   destination_cidr_block = "0.0.0.0/0"
-  gateway_id = aws_internet_gateway.igw.id
+  gateway_id             = aws_internet_gateway.igw.id
 }
 
 resource "aws_eip" "ngw" {
-  count = length(local.public_subnet_ids)
+  count  = length(local.public_subnet_ids)
   domain = "vpc"
 }
 
 resource "aws_nat_gateway" "nwg" {
-  count = length(local.public_subnet_ids)
-  allocation_id = element(aws_eip.ngw.*.id, count.index )
-  subnet_id     = element(local.public_subnet_ids, count.index )
+  count         = length(local.public_subnet_ids)
+  allocation_id = element(aws_eip.ngw.*.id, count.index)
+  subnet_id     = element(local.public_subnet_ids, count.index)
 }
 
 resource "aws_route" "ngw" {
-  count = length(local.private_subnet_ids)
-  route_table_id = element(local.private_subnet_ids, count.index )
+  count                  = length(local.private_subnet_ids)
+  route_table_id         = element(local.private_subnet_ids, count.index)
   destination_cidr_block = "0.0.0.0/0"
-  nat_gateway_id = element(aws_nat_gateway.nwg.*.id, count.index )
+  nat_gateway_id         = element(aws_nat_gateway.nwg.*.id, count.index)
 }
-
-
-
-
 
 
 # To fetch the output of subnets child module output.tf.
