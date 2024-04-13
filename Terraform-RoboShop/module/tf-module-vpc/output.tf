a632354b1bfe5d532fe87a84568f183bf02f20b3
Abhijeet Pramanik <abhijeet4022@gmail.com>
2024-04-13 19:56:13 +0530

    RoboShop-Project

--- Terraform-RoboShop/module/tf-module-vpc/output.tf
+++ Terraform-RoboShop/module/tf-module-vpc/output.tf
@@ -8,6 +8,9 @@
 #  value = var.all_subnets
 #}
 
+output "public_subnets_ids" {
+  value = local.public_subnet_ids
+}
 
 # Sending the output
 output "private_vpc_id" {
@@ -15,7 +18,7 @@ output "private_vpc_id" {
 }
 
 output "vpc_id" {
-  value = aws_vpc.main
+  value = aws_vpc.main.id
 }
 
 output "subnets" {
