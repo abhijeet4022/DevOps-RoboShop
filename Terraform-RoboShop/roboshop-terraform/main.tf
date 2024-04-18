989a6f9a7af8ca8e50ce45520cc3b1a99bf08e2f
Abhijeet Pramanik <abhijeet4022@gmail.com>
2024-04-18 17:00:43 +0530

    RoboShop-Project

--- Terraform-RoboShop/roboshop-terraform/main.tf
+++ Terraform-RoboShop/roboshop-terraform/main.tf
@@ -15,20 +15,20 @@ module "vpc" {
 
 #
 ## ALB Creation
-module "alb" {
-  source             = "../module/tf-module-alb"
-  for_each           = var.alb
-  tags               = var.tags
-  env                = var.env
-  internal           = each.value["internal"]
-  load_balancer_type = each.value["load_balancer_type"]
-  sg_ingress_cidr    = each.value["sg_ingress_cidr"]
-  # condition ? true_val : false_val
-  # private vpc id we will fetch by lookup func from output block of vpc module.
-  vpc_id             = each.value["internal"] ? local.vpc_id : var.default_vpc_id
-  subnets            = each.value["internal"] ? local.app_subnets : data.aws_subnets.subnets.ids
-  sg_port            = each.value["sg_port"]
-}
+#module "alb" {
+#  source             = "../module/tf-module-alb"
+#  for_each           = var.alb
+#  tags               = var.tags
+#  env                = var.env
+#  internal           = each.value["internal"]
+#  load_balancer_type = each.value["load_balancer_type"]
+#  sg_ingress_cidr    = each.value["sg_ingress_cidr"]
+#  # condition ? true_val : false_val
+#  # private vpc id we will fetch by lookup func from output block of vpc module.
+#  vpc_id             = each.value["internal"] ? local.vpc_id : var.default_vpc_id
+#  subnets            = each.value["internal"] ? local.app_subnets : data.aws_subnets.subnets.ids
+#  sg_port            = each.value["sg_port"]
+#}
 
 
 # DocumentDB Creation
