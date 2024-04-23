4f56fb1654fe9778fd92d3cf45010acf2055ce69
Abhijeet Pramanik <abhijeet4022@gmail.com>
2024-04-23 16:06:10 +0530

    RoboShop-Project

--- Terraform-RoboShop/roboshop-terraform/main.tf
+++ Terraform-RoboShop/roboshop-terraform/main.tf
@@ -51,26 +51,26 @@ module "vpc" {
 #}
 
 # Aurora_MYSQL RDS Creation.
-module "rds" {
-  source = "../module/tf-module-rds"
-  tags   = var.tags
-  env    = var.env
-
-  for_each                = var.rds
-  vpc_id                  = local.vpc_id
-  db_subnets_ids          = local.db_subnets_ids
-  app_subnets_cidr        = local.app_subnets_cidr
-  backup_retention_period = each.value["backup_retention_period"]
-  engine                  = each.value["engine"]
-  engine_version          = each.value["engine_version"]
-  family                  = each.value["family"]
-  instance_class          = each.value["instance_class"]
-  instance_count          = each.value["instance_count"]
-  preferred_backup_window = each.value["preferred_backup_window"]
-  rds_type                = each.value["rds_type"]
-  sg_port                 = each.value["sg_port"]
-  skip_final_snapshot     = each.value["skip_final_snapshot"]
-}
+#module "rds" {
+#  source = "../module/tf-module-rds"
+#  tags   = var.tags
+#  env    = var.env
+#
+#  for_each                = var.rds
+#  vpc_id                  = local.vpc_id
+#  db_subnets_ids          = local.db_subnets_ids
+#  app_subnets_cidr        = local.app_subnets_cidr
+#  backup_retention_period = each.value["backup_retention_period"]
+#  engine                  = each.value["engine"]
+#  engine_version          = each.value["engine_version"]
+#  family                  = each.value["family"]
+#  instance_class          = each.value["instance_class"]
+#  instance_count          = each.value["instance_count"]
+#  preferred_backup_window = each.value["preferred_backup_window"]
+#  rds_type                = each.value["rds_type"]
+#  sg_port                 = each.value["sg_port"]
+#  skip_final_snapshot     = each.value["skip_final_snapshot"]
+#}
 
 # ElastiCache Cluster Creation.
 module "elasticache" {
