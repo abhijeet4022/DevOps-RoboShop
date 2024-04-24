6fb221ea2abf8267987337e16ae919797e7547e0
Abhijeet Pramanik <abhijeet4022@gmail.com>
2024-04-24 17:18:02 +0530

    RoboShop-Project

--- Terraform-RoboShop/module/tf-module-app/main.tf
+++ Terraform-RoboShop/module/tf-module-app/main.tf
@@ -62,7 +62,7 @@ resource "aws_autoscaling_group" "main" {
   }
 
   tag {
-    key                 = Name
+    key                 = "Name"
     value               = local.name_prefix
     propagate_at_launch = true
   }
