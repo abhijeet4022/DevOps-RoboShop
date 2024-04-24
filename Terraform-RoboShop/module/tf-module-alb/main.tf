aa599245c24489193159b0f4d1751c9b9e21a3e8
Abhijeet Pramanik <abhijeet4022@gmail.com>
2024-04-24 18:55:02 +0530

    RoboShop-Project

--- Terraform-RoboShop/module/tf-module-alb/main.tf
+++ Terraform-RoboShop/module/tf-module-alb/main.tf
@@ -8,6 +8,23 @@ resource "aws_lb" "main" {
   tags               = merge(local.tags, { Name = local.alb_name })
 }
 
+# Listener Creation
+resource "aws_lb_listener" "main" {
+  load_balancer_arn = aws_lb.main.arn
+  port              = "80"
+  protocol          = "HTTP"
+
+  default_action {
+    type = "fixed-response"
+
+    fixed_response {
+      content_type = "text/plain"
+      message_body = "ERROR"
+      status_code  = "400"
+    }
+  }
+}
+
 
 # Security Group Creation
 resource "aws_security_group" "main" {
