151b3db03198fb14349ccd7c9ecf34ec2e2ba935
Abhijeet Pramanik <abhijeet4022@gmail.com>
2024-04-25 17:46:53 +0530

    RoboShop-Project

--- Terraform-RoboShop/module/tf-module-app/main.tf
+++ Terraform-RoboShop/module/tf-module-app/main.tf
@@ -76,7 +76,7 @@ resource "aws_route53_record" "main" {
 }
 
 
-# Target Group Create for LB.
+# Target Group Create for Private LB.
 resource "aws_lb_target_group" "main" {
   name     = local.name_prefix
   port     = var.sg_port
@@ -99,4 +99,13 @@ resource "aws_lb_listener_rule" "main" {
       values = [var.component == "frontend" ? "${var.env}.learntechnology.cloud" : "${var.component}-${var.env}.learntechnology.cloud"]
     }
   }
+}
+
+# Target Group Create for Public LB.
+resource "aws_lb_target_group" "public" {
+  count = var.component == "frontend" ? 1 : 0
+  name     = "${local.name_prefix}-public"
+  port     = var.sg_port
+  protocol = "HTTP"
+  vpc_id   = var.vpc_id
 }
