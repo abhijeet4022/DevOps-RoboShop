87740c0a20fcb23337caa226dcb97dd418e3681c
Abhijeet Pramanik <abhijeet4022@gmail.com>
2024-04-25 19:12:20 +0530

    RoboShop-Project

--- Terraform-RoboShop/module/tf-module-app/main.tf
+++ Terraform-RoboShop/module/tf-module-app/main.tf
@@ -117,7 +117,7 @@ resource "aws_lb_target_group" "public" {
 resource "aws_lb_target_group_attachment" "public" {
   # This will iterate only for frontend.
   count             = var.component == "frontend" ? length(tolist(data.dns_a_record_set.private_alb.addrs)) : 0
-  target_group_arn  = aws_lb_target_group.public.arn
+  target_group_arn  = aws_lb_target_group.public[0].arn
   target_id         = element(tolist(data.dns_a_record_set.private_alb.addrs), count.index)
   port              = 80
   availability_zone = "all"
