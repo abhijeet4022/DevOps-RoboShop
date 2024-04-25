e6d5697c59efa3ba40342d0b980fd9302327d9fa
Abhijeet Pramanik <abhijeet4022@gmail.com>
2024-04-25 19:10:09 +0530

    RoboShop-Project

--- Terraform-RoboShop/module/tf-module-app/main.tf
+++ Terraform-RoboShop/module/tf-module-app/main.tf
@@ -125,6 +125,7 @@ resource "aws_lb_target_group_attachment" "public" {
 
 # Create listener rule for dev. redirect the dev.learntechnology.cloud traffic to traget gruop representing the private lb ip.
 resource "aws_lb_listener_rule" "public" {
+  count        = var.component == "frontend" ? 1 : 0
   listener_arn = var.public_listener
   priority     = var.priority
 
