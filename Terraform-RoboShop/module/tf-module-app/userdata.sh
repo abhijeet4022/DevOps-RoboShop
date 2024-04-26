c7f1a0cef66bd1ad69e2c7094850160b20d2442b
Abhijeet Pramanik <abhijeet4022@gmail.com>
2024-04-26 16:29:05 +0530

    RoboShop-Project

--- Terraform-RoboShop/module/tf-module-app/userdata.sh
+++ Terraform-RoboShop/module/tf-module-app/userdata.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
-yum install ansible bash-completion -y &>> /opt/userdata.log
+yum install ansible bash-completion python3.11-pip.noarch -y &>> /opt/userdata.log
+pip3.11 install botocore boto3
 ansible-pull -i localhost, -U https://github.com/abhijeet4022/DevOps-RoboShop.git Ansible-RoboShop/main.yml -e component=${component} &>> /opt/userdata.log
