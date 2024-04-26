e9ef31027045183d6b8228ddbb3350fc95b7def9
Abhijeet Pramanik <abhijeet4022@gmail.com>
2024-04-26 15:10:40 +0530

    RoboShop-Project

--- Terraform-RoboShop/module/tf-module-app/main.tf
+++ Terraform-RoboShop/module/tf-module-app/main.tf
@@ -114,29 +114,29 @@ resource "aws_lb_target_group" "public" {
 }
 
 # Attach the Private LB IP with Above TG.
-resource "aws_lb_target_group_attachment" "public" {
-  # This will iterate only for frontend.
-  count             = var.component == "frontend" ? length(tolist(data.dns_a_record_set.private_alb.addrs)) : 0
-  target_group_arn  = aws_lb_target_group.public[0].arn
-  target_id         = element(tolist(data.dns_a_record_set.private_alb.addrs), count.index)
-  port              = 80
-  availability_zone = "all"
-}
+#resource "aws_lb_target_group_attachment" "public" {
+#  # This will iterate only for frontend.
+#  count             = var.component == "frontend" ? length(tolist(data.dns_a_record_set.private_alb.addrs)) : 0
+#  target_group_arn  = aws_lb_target_group.public[0].arn
+#  target_id         = element(tolist(data.dns_a_record_set.private_alb.addrs), count.index)
+#  port              = 80
+#  availability_zone = "all"
+#}
 
-# Create listener rule for dev. redirect the dev.learntechnology.cloud traffic to traget gruop representing the private lb ip.
-resource "aws_lb_listener_rule" "public" {
-  count        = var.component == "frontend" ? 1 : 0
-  listener_arn = var.public_listener
-  priority     = var.priority
-
-  action {
-    type             = "forward"
-    target_group_arn = aws_lb_target_group.public[0].arn
-  }
-
-  condition {
-    host_header {
-      values = ["${var.env}.learntechnology.cloud"]
-    }
-  }
-}
+## Create listener rule for dev. redirect the dev.learntechnology.cloud traffic to traget gruop representing the private lb ip.
+#resource "aws_lb_listener_rule" "public" {
+#  count        = var.component == "frontend" ? 1 : 0
+#  listener_arn = var.public_listener
+#  priority     = var.priority
+#
+#  action {
+#    type             = "forward"
+#    target_group_arn = aws_lb_target_group.public[0].arn
+#  }
+#
+#  condition {
+#    host_header {
+#      values = ["${var.env}.learntechnology.cloud"]
+#    }
+#  }
+#}
