40c680ae1dcf94a2777dfb1aec24cb59df166b77
Abhijeet Pramanik <abhijeet4022@gmail.com>
2024-05-11 16:31:04 +0530

    Jekins

--- Jenkins-Roboshop/shared-library/vars/demo.groovy
+++ Jenkins-Roboshop/shared-library/vars/demo.groovy
@@ -5,3 +5,35 @@ def info(message) {
 def warning(message) {
     echo "WARNING: ${message}"
 }
+
+def call() {
+    pipeline {
+        agent { node { label 'workstation' } }
+        stages {
+            stage('Compile Code') {
+                steps {
+                    echo 'Compiling The Code.'
+                    script {
+                        info 'Starting'
+                        warning 'Nothing to do!'
+                    }
+                }
+            }
+            stage('Test') {
+                steps {
+                    echo 'Test The Code.'
+                }
+            }
+            stage('Code Quality') {
+                steps {
+                    echo 'Code Quality'
+                }
+            }
+            stage('Code Security') {
+                steps {
+                    echo 'Code Security'
+                }
+            }
+        }
+    }
+}
