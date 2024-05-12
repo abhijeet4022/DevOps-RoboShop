e73ce132db3df57aa6eb25492148607685dbda95
Abhijeet Pramanik <abhijeet4022@gmail.com>
2024-05-12 14:33:10 +0530

    Jekins

--- Jenkins-Roboshop/shared-library/vars/cipipeline.groovy
+++ Jenkins-Roboshop/shared-library/vars/cipipeline.groovy
@@ -2,6 +2,7 @@ def call() {
     node('workstation') {
 
         sh "find . | sed -e '1d' | xargs rm -rf "
+        git branch: 'main', url: "https://github.com/abhijeet4022/${component}"
 
 
         stage('Complie Code'){
