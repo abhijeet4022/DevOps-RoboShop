d38cea1b04a264ea6c415b0fa45edc60942f8819
Abhijeet Pramanik <abhijeet4022@gmail.com>
2024-05-13 07:15:10 +0530

    Jekins

--- Jenkins-Roboshop/shared-library/vars/cipipeline.groovy
+++ Jenkins-Roboshop/shared-library/vars/cipipeline.groovy
@@ -2,7 +2,18 @@ def call() {
     node('workstation') {
 
         sh "find . | sed -e '1d' | xargs rm -rf "
-        git branch: 'main', url: "https://github.com/abhijeet4022/${component}"
+
+        // Continuous download the developer code for application from repo for build test.
+        // If tag is specified in repo then download the code from tag if not then from branch..
+        if (env.TAG_NAME ==~ ".*") {
+            env.branch_name = "refs/tags/${env.TAG_NAME}"
+        } else {
+            env.branch_name = "${env.BRANCH_NAME}"
+        }
+        checkout scmGit(
+                branches: [[name: branch_name]],
+                userRemoteConfigs: [[url: "https://github.com/abhijeet4022/${component}"]]
+        )
 
 
         stage('Complie Code'){
