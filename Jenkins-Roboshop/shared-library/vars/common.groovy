a8bacc9c994ba0af730a02a00e2f1aa1ad0a270c
Abhijeet Pramanik <abhijeet4022@gmail.com>
2024-05-16 19:46:22 +0530

    Jekins

--- Jenkins-Roboshop/shared-library/vars/common.groovy
+++ Jenkins-Roboshop/shared-library/vars/common.groovy
@@ -41,11 +41,23 @@ def codeQuality() {
     stage('Code Quality') {
         env.sonaruser = sh(script: 'aws ssm get-parameter --name "sonarqube.username" --with-decryption --query="Parameter.Value" |xargs', returnStdout: true).trim()
         env.sonarpass = sh(script: 'aws ssm get-parameter --name "sonarqube.password" --with-decryption --query="Parameter.Value" |xargs', returnStdout: true).trim()
-        wrap([$class: "MaskPasswordsBuildWrapper", varPasswordPairs: [[password: sonarpass]]]) {
-            sh 'sonar-scanner -Dsonar.host.url=http://172.31.18.50:9000 -Dsonar.login=${sonaruser} -Dsonar.password=${sonarpass} -Dsonar.projectKey=${component} -Dsonar.qualitygate.wait=true'
+
+
+        if (codeType == 'maven') {
+            wrap([$class: "MaskPasswordsBuildWrapper", varPasswordPairs: [[password: sonarpass]]]) {
+                sh 'sonar-scanner -Dsonar.host.url=http://172.31.18.50:9000 -Dsonar.login=${sonaruser} -Dsonar.password=${sonarpass} -Dsonar.projectKey=${component} -Dsonar.qualitygate.wait=true -Dsonar.java.binaries=./target'
+            }
+
+
+        } else {
+            wrap([$class: "MaskPasswordsBuildWrapper", varPasswordPairs: [[password: sonarpass]]]) {
+                sh 'sonar-scanner -Dsonar.host.url=http://172.31.18.50:9000 -Dsonar.login=${sonaruser} -Dsonar.password=${sonarpass} -Dsonar.projectKey=${component} -Dsonar.qualitygate.wait=true'
+            }
 
 
         }
+
+
     }
 }
 
