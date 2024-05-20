9e516fa2cb7957fce576b2743a338e0c5fab1115
Abhijeet Pramanik <abhijeet4022@gmail.com>
2024-05-20 17:04:02 +0530

    Jekins

--- Terraform-RoboShop/module/roboshop-aws-parameter/main.tf
+++ Terraform-RoboShop/module/roboshop-aws-parameter/main.tf
@@ -1,8 +1,9 @@
 # Parameter Creation
 resource "aws_ssm_parameter" "main" {
   for_each = var.parameters
-  name  = each.key
-  type  = each.value["type"]
-  value = each.value["value"]
+  name     = each.key
+  type     = each.value["type"]
+  value    = each.value["value"]
+  key_id   = "arn:aws:kms:us-east-1:767398040211:key/2e711605-a22c-4302-9087-c8ef84499250"
 }
 
