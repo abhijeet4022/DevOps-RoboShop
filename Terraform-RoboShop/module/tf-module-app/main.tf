9f03b3278b577a9edb1cd4425a1ef17cacb1851f
Abhijeet Pramanik <abhijeet4022@gmail.com>
2024-05-23 18:49:26 +0530

    Jekins

--- Terraform-RoboShop/module/tf-module-app/main.tf
+++ Terraform-RoboShop/module/tf-module-app/main.tf
@@ -53,6 +53,9 @@ resource "aws_launch_template" "main" {
   image_id               = data.aws_ami.ami.id
   instance_type          = var.instance_type
   vpc_security_group_ids = [aws_security_group.main.id]
+  monitoring {
+    enabled = true
+  }
   iam_instance_profile {
     name = "${local.name_prefix}-profile"
   }
