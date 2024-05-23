d8dfc872012f36472a0c3ca8d098f49c6fce035a
Abhijeet Pramanik <abhijeet4022@gmail.com>
2024-05-23 17:20:27 +0530

    Jekins

--- Terraform-RoboShop/module/tf-module-app/main.tf
+++ Terraform-RoboShop/module/tf-module-app/main.tf
@@ -78,7 +78,7 @@ resource "aws_autoscaling_group" "main" {
   max_size                  = var.max_size
   min_size                  = var.min_size
   health_check_grace_period = 300
-  health_check_type         = "ELB"
+  health_check_type         = var.component == "dispatch" ? "EC2" : "ELB"
   force_delete              = true
   target_group_arns         = [aws_lb_target_group.main.arn]
   launch_template {
