7dfcd496e59d4d1fd6942df3ab194f59a13e3618
Abhijeet Pramanik <abhijeet4022@gmail.com>
2024-05-25 16:01:23 +0530

    Jekins

--- Jenkins-Roboshop/shared-library/vars/deploy.groovy
+++ Jenkins-Roboshop/shared-library/vars/deploy.groovy
@@ -1,7 +1,6 @@
 def call() {
 
     pipeline {
-
         agent any
 
         options {
@@ -9,16 +8,35 @@ def call() {
             buildDiscarder(logRotator(numToKeepStr: '3'))
         }
 
+        parameters {
+            string(name: 'COMPONENT', defaultValue: '', description: 'Which component to deploy?')
+            string(name: 'VERSION', defaultValue: '', description: 'Which version to deploy?')
+            string(name: 'ENV', defaultValue: '', description: 'Which environment to deploy?')
+        }
+
         environment {
             SSH = credentials("centos-ssh")
         }
 
         stages {
-            stage('SSH') {
+            stage('Parameter Store Update') {
                 steps {
-                    echo "${SSH_PSW}"
-                    echo "${SSH_USR}"
+                    sh '''
+                        aws ssm put-parameter --name "${COMPONENT}.${ENV}.appVersion" --type "String" --value "${VERSION}" --overwrite
+'''
+                    script {
+                        addShortText(text: "${ENV}-${COMPONENT}-${VERSION}")
+                    }
                 }
+            }
+
+            stage('Application Deployment') {
+                sh '''
+                    aws ec2 describe-instances --filters "Name=tag:Name,Values=prod-shipping" --query 'Reservations[*].Instances[*].[PrivateIpAddress]' --output text > inv
+                    ansible-playbook -i inv Ansible-RoboShop/main.yml -e ansible_user=${SSH_USR} -e ansible_password=${SSH_PSW} -e component=${COMPONENT} -e env=${ENV}
+'''
+
+
             }
         }
 
